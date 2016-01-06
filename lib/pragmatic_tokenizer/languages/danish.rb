77bf9552bf8458fa31535ee966bfd850e27dfdf7
Kevin Dias <diasks2@gmail.com>
2016-01-06 14:09:28 +0900

    Add CONTRACTIONS for every language

--- lib/pragmatic_tokenizer/languages/danish.rb
+++ lib/pragmatic_tokenizer/languages/danish.rb
@@ -4,6 +4,7 @@ module PragmaticTokenizer
       include Languages::Common
       ABBREVIATIONS = []
       STOP_WORDS = ["De", "I", "af", "aldrig", "alle", "altid", "bagved", "de", "der", "du", "efter", "eller", "en", "endnu", "et", "fjernt", "for", "foran", "fra", "få", "gennem", "god", "han", "her", "hos", "hovfor", "hun", "hurtig", "hvad", "hvem", "hvonår", "hvor", "hvordan", "hvorhen", "imod", "ja", "jeg", "langsom", "lidt", "mange", "med", "meget", "mellem", "mere", "mindre", "måske", "nede", "nej", "nok", "nu", "når", "og", "oppe", "på", "rask", "sammen", "temmelig", "til", "uden", "udenfor", "under", "ved", "vi"]
+      CONTRACTIONS = {}
     end
   end
 end
