2a07d400b0c83b19b254b80d7292648dd87c6042
a@k27.net <a@k27.net>
2016-01-23 02:38:55 +0100

    using new lambda literal syntax

--- spec/pragmatic_tokenizer_spec.rb
+++ spec/pragmatic_tokenizer_spec.rb
@@ -7,35 +7,35 @@ describe PragmaticTokenizer do
 
   describe '#initialize' do
     it 'raises an error if the text argument is nil' do
-      lambda { expect(PragmaticTokenizer::Tokenizer.new(nil, language: 'en').tokenize).to raise_error }
+      -> { expect(PragmaticTokenizer::Tokenizer.new(nil, language: 'en').tokenize).to raise_error }
     end
 
     it 'raises an error if the text argument is empty' do
-      lambda { expect(PragmaticTokenizer::Tokenizer.new('', language: 'en').tokenize).to raise_error }
+      -> { expect(PragmaticTokenizer::Tokenizer.new('', language: 'en').tokenize).to raise_error }
     end
 
     it 'raises an error if minimum_length is not an Integer' do
-      lambda { expect(PragmaticTokenizer::Tokenizer.new("heelo", minimum_length: "strawberry").tokenize).to raise_error }
+      -> { expect(PragmaticTokenizer::Tokenizer.new("heelo", minimum_length: "strawberry").tokenize).to raise_error }
     end
 
     it 'raises an error if long_word_split is not an Integer' do
-      lambda { expect(PragmaticTokenizer::Tokenizer.new("heeloo", long_word_split: "yes!").tokenize).to raise_error }
+      -> { expect(PragmaticTokenizer::Tokenizer.new("heeloo", long_word_split: "yes!").tokenize).to raise_error }
     end
 
     it 'raises an error if the text is not a String' do
-      lambda { expect(PragmaticTokenizer::Tokenizer.new(5).tokenize).to raise_error }
+      -> { expect(PragmaticTokenizer::Tokenizer.new(5).tokenize).to raise_error }
     end
 
     it "raises an error if the punctuation argument is not nil, 'all', 'semi', or 'none'" do
-      lambda { expect(PragmaticTokenizer::Tokenizer.new('', language: 'en', punctuation: 'world').tokenize).to raise_error }
+      -> { expect(PragmaticTokenizer::Tokenizer.new('', language: 'en', punctuation: 'world').tokenize).to raise_error }
     end
 
     it "raises an error if the numbers argument is not nil, 'all', 'semi', or 'none'" do
-      lambda { expect(PragmaticTokenizer::Tokenizer.new('', language: 'en', numbers: 'world').tokenize).to raise_error }
+      -> { expect(PragmaticTokenizer::Tokenizer.new('', language: 'en', numbers: 'world').tokenize).to raise_error }
     end
 
     it "raises an error if the mentions argument is not nil, 'all', 'semi', or 'none'" do
-      lambda { expect(PragmaticTokenizer::Tokenizer.new('', language: 'en', mentions: 'world').tokenize).to raise_error }
+      -> { expect(PragmaticTokenizer::Tokenizer.new('', language: 'en', mentions: 'world').tokenize).to raise_error }
     end
   end
 end
