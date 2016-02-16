dcb1a87ffd1de5aff17b9e701921fed923564fe9
Kevin Dias <diasks2@gmail.com>
2016-02-16 09:21:03 +0900

    Require set

--- lib/pragmatic_tokenizer/tokenizer.rb
+++ lib/pragmatic_tokenizer/tokenizer.rb
@@ -4,6 +4,7 @@ require 'pragmatic_tokenizer/pre_processor'
 require 'pragmatic_tokenizer/post_processor'
 require 'pragmatic_tokenizer/full_stop_separator'
 require 'unicode'
+require 'set'
 
 module PragmaticTokenizer
   class Tokenizer
